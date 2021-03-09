8f801062624a807624c00cc2f49db1f9a50efbd1
ShaunAshraf <33549886+meavik@users.noreply.github.com>
2021-03-09 23:02:16 +0600

    Create sumOfNodes.c

---
+++ sumOfNodes.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+    int data;
+    struct Node*link;
+}*head=0;
+
+void CreateList(){
+
+    struct Node *temp,*newNode;
+    int choice;
+    do{
+        newNode =(struct Node*)malloc(sizeof(struct Node));
+        printf("Enter the data: ");
+        scanf("%d",&newNode->data);
+        newNode->link = 0;
+        if(head==0){
+            head = temp = newNode;
+        }
+        else{
+            temp->link = newNode;
+            temp = newNode;
+        }
+        printf("Continue inserting data?(Enter 1 to continue): ");
+        scanf("%d",&choice);
+    }while(choice == 1);
+}
+
+void display(struct Node*p){
+    printf("\nDisplaying data: ");
+    while(p){
+        printf("%d ",p->data);
+        p=p->link;
+    }
+    printf("\n");
+}
+
+int sumOfValues(struct Node*p){
+    int sum = 0;
+    if(p==0){
+        return 0;
+    }
+    sum = p->data + sumOfValues(p->link);
+    return sum ;
+}
+
+int main(){
+
+    CreateList();
+    display(head);
+    printf("\nSum is: %d\n",sumOfValues(head));
+
+    return 0;
+}
